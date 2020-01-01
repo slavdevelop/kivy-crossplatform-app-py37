b8776d971ed648d2f93c54e88a49728ab20f356e
Slavi Shtipkov <development@shtipkov.com>
2020-01-02 00:35:36 +0200

    Created simple inputs, submit button and grid

--- main.py
+++ main.py
@@ -1,11 +1,51 @@
 import kivy
 from kivy.app import App
 from kivy.uix.label import Label
+from kivy.uix.gridlayout import GridLayout
+from kivy.uix.textinput import TextInput
+from kivy.uix.button import Button
+
+
+class MyGrid(GridLayout):
+    def __init__(self, **kwargs):
+        super(MyGrid, self).__init__(**kwargs)
+        self.cols = 1
+
+        self.inside = GridLayout()
+        self.inside.cols = 2
+
+        self.inside.add_widget(Label(text="First Name: "))
+        self.firstName = TextInput(multiline=False)
+        self.inside.add_widget(self.firstName)
+
+        self.inside.add_widget(Label(text="Last Name: "))
+        self.lastName = TextInput(multiline=False)
+        self.inside.add_widget(self.lastName)
+
+        self.inside.add_widget(Label(text="Email: "))
+        self.email = TextInput(multiline=False)
+        self.inside.add_widget(self.email)
+
+        self.add_widget(self.inside)
+
+        self.submit = Button(text="Submit", font_size=40)
+        self.submit.bind(on_press=self.pressed)
+        self.add_widget(self.submit)
+
+    def pressed(self, instance):
+        fName = self.firstName.text
+        lName = self.lastName.text
+        email = self.email.text
+
+        print("First Name: ", fName, "Last Name: ", lName, "Email: ", email)
+        self.firstName.text = ""
+        self.lastName.text = ""
+        self.email.text = ""
 
 
 class MyApp(App):
     def build(self):
-        return Label(text="Slav Dev")
+        return MyGrid()
 
 
 if __name__ == "__main__":
