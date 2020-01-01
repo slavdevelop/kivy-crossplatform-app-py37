3cdb47ccd3aeb855ccbbafb92057047aaeb8edde
Slavi Shtipkov <development@shtipkov.com>
2020-01-02 00:48:36 +0200

    Implementing the same inputs with .kv file

--- main.py
+++ main.py
@@ -4,43 +4,11 @@ from kivy.uix.label import Label
 from kivy.uix.gridlayout import GridLayout
 from kivy.uix.textinput import TextInput
 from kivy.uix.button import Button
+from kivy.uix.widget import Widget
 
 
-class MyGrid(GridLayout):
-    def __init__(self, **kwargs):
-        super(MyGrid, self).__init__(**kwargs)
-        self.cols = 1
-
-        self.inside = GridLayout()
-        self.inside.cols = 2
-
-        self.inside.add_widget(Label(text="First Name: "))
-        self.firstName = TextInput(multiline=False)
-        self.inside.add_widget(self.firstName)
-
-        self.inside.add_widget(Label(text="Last Name: "))
-        self.lastName = TextInput(multiline=False)
-        self.inside.add_widget(self.lastName)
-
-        self.inside.add_widget(Label(text="Email: "))
-        self.email = TextInput(multiline=False)
-        self.inside.add_widget(self.email)
-
-        self.add_widget(self.inside)
-
-        self.submit = Button(text="Submit", font_size=40)
-        self.submit.bind(on_press=self.pressed)
-        self.add_widget(self.submit)
-
-    def pressed(self, instance):
-        fName = self.firstName.text
-        lName = self.lastName.text
-        email = self.email.text
-
-        print("First Name: ", fName, "Last Name: ", lName, "Email: ", email)
-        self.firstName.text = ""
-        self.lastName.text = ""
-        self.email.text = ""
+class MyGrid(Widget):
+    pass
 
 
 class MyApp(App):
